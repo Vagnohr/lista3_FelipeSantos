abb5fdd117be21b0a06038438f542cddee7fe5a3
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-14 20:23:51 -0300

    Update atividade2.py

--- atividade2.py
+++ atividade2.py
@@ -5,8 +5,14 @@ if peso < 52:
 elif peso >=52 and peso <65:
     print("O seu lutador está na categoria pena")
 elif peso >=65 and peso <72:
-elif peso >=72 and peso <79
-elif peso >=79 and peso <86
-elif peso >=86 and peso <90
-elif peso >=90 and peso <100
-elif peso >=100
+    print("O seu lutador está na categoria leve")
+elif peso >=72 and peso <79:
+    print("O seu lutador está na categoria ligeiro")
+elif peso >=79 and peso <86:
+    print("O seu lutador está na categoria meio-médio")
+elif peso >=86 and peso <90:
+    print("O seu lutador está na categoria médio")
+elif peso >=90 and peso <100:
+    print("O seu lutador está na categoria meio-pesado")
+elif peso >=100:
+    print("O seu lutador está na categoria pesado")
