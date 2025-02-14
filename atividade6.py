be6d47ceaa144a6a986eca7d212736455399b78c
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-14 20:59:51 -0300

    Update atividade6.py

--- atividade6.py
+++ atividade6.py
@@ -2,16 +2,16 @@ nome = input("Insira o nome do municipio: ")
 eleitores = int(input("Insira a quantidade de eleitores: "))
 eleitor_votos = int(input("Insira quantos votos o eleitor recebeu: "))
 votos = eleitores/2
-if eleitores >= 200000:
+if eleitor_votos > eleitores:
+    print("quantidade de votos invalida")
+    print("Felipe Bortoluzz dos Santos")
+elif eleitores >= 200000:
     if eleitor_votos < votos:
         print("Havera segundo turno")
         print("Felipe Bortoluzz dos Santos")
     elif eleitor_votos >= votos:
         print("Não havera segundo turno")
         print("Felipe Bortoluzz dos Santos")
-elif eleitor_votos > eleitores:
-    print("quantidade de votos invalida")
-    print("Felipe Bortoluzz dos Santos")
 else:
     print("não há eleitores suficientes para segundo turno")
     print("Felipe Bortoluzz dos Santos")
