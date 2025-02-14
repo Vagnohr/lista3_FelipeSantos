e2d82c003c97c01df07a8b03e54b99879ba15b2c
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-14 20:50:02 -0300

    Update atividade1.py

--- atividade1.py
+++ atividade1.py
@@ -3,12 +3,17 @@ valor = int(input("Insira a quantia de dinheiro que quer investir: "))
 if risco == "BX":
     if valor <=1000:
         print("envista em uma conta poupança")
+        print("Felipe Bortoluzz dos Santos")
     else:
         print("envista em bitcoin")
+        print("Felipe Bortoluzz dos Santos")
 elif risco == "AL":
     if valor <=1000:
         print("envista em uma renda fixa")
+        print("Felipe Bortoluzz dos Santos")
     else:
         print("envista em ações")
+        print("Felipe Bortoluzz dos Santos")
 else:
     print("informação invalida")
+    print("Felipe Bortoluzz dos Santos")
