1dac55e81306f58453b7c5ca6f4a0802f79e29bc
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-14 20:48:13 -0300

    Update atividade5.py

--- atividade5.py
+++ atividade5.py
@@ -2,6 +2,8 @@ custo = int(input("Insira o preço de custo do produto: "))
 if custo <=100:
     margem_bruta = (custo/100)*45
     print("O preço de venda do produto é {}".format(margem_bruta))
+    print("Felipe Bortoluzz dos Santos")
 elif custo >100:
     margem_bruta = (custo/100)*35
     print("O preço de venda do produto é {}".format(margem_bruta))
+    print("Felipe Bortoluzz dos Santos")
