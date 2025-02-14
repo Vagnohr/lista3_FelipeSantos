9ad43ca2dc66eb683d2e39cf40b91e877621c331
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-14 20:48:40 -0300

    Update atividade4.py

--- atividade4.py
+++ atividade4.py
@@ -4,12 +4,17 @@ sexo = input("Insira o seu sexo: ")
 if "f" in sexo or "F" in sexo:
     if idade >=21 and idade <=34:
         print("Você sera aceita para servir")
+        print("Felipe Bortoluzz dos Santos")
     else:
         print("Você não sera aceita para servir")
+        print("Felipe Bortoluzz dos Santos")
 elif "m" in sexo or "M" in sexo:
     if idade >=18 and idade <=39:
         print("Você sera aceito para servir")
+        print("Felipe Bortoluzz dos Santos")
     else:
         print("Você não sera aceito para servir")
+        print("Felipe Bortoluzz dos Santos")
 else:
     print("sexo invalido")
+    print("Felipe Bortoluzz dos Santos")
