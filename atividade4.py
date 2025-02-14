174a51e353553d9f0e0c37cd0d1494a263c9b47f
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-14 20:44:50 -0300

    Update atividade4.py

--- atividade4.py
+++ atividade4.py
@@ -1,14 +1,14 @@
-nome = print("Insira o seu nome: ")
-idade = print("Insira a sua idade: ")
-sexo = print("Insira o seu sexo: ")
-if "f" or "F" in sexo:
+nome = input("Insira o seu nome: ")
+idade = int(input("Insira a sua idade: "))
+sexo = input("Insira o seu sexo: ")
+if "f" in sexo or "F" in sexo:
     if idade >=21 and idade <=34:
         print("Você sera aceita para servir")
     else:
         print("Você não sera aceita para servir")
-elif "m" or "M" in sexo:
+elif "m" in sexo or "M" in sexo:
     if idade >=18 and idade <=39:
-        print("Você sera aceita para servir")
+        print("Você sera aceito para servir")
     else:
         print("Você não sera aceito para servir")
 else:
