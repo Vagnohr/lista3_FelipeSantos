12ad60355cf1cc2860c232dbcacdaf9f1eec1cb2
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-14 20:47:52 -0300

    Update atividade6.py

--- atividade6.py
+++ atividade6.py
@@ -5,9 +5,13 @@ votos = eleitores/2
 if eleitores >= 200000:
     if eleitor_votos < votos:
         print("Havera segundo turno")
+        print("Felipe Bortoluzz dos Santos")
     elif eleitor_votos >= votos:
         print("Não havera segundo turno")
+        print("Felipe Bortoluzz dos Santos")
 elif eleitor_votos > eleitores:
     print("quantidade de votos invalida")
+    print("Felipe Bortoluzz dos Santos")
 else:
     print("não há eleitores suficientes para segundo turno")
+    print("Felipe Bortoluzz dos Santos")
