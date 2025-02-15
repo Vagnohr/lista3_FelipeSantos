1d3e71812581f86ad51bc032a876f27800baccb4
Felipe B. Santos <felipebtst1@gmail.com>
2025-02-14 21:01:31 -0300

    Update atividade3.py

--- atividade3.py
+++ atividade3.py
@@ -1,9 +1,9 @@
 salario = int(input("Insira a quantia do seu salario: "))
 imprestimo = int(input("Insira a quantia do seu imprestimo: "))
 conta = (salario/100)*8
-if conta >= imprestimo:
+if conta < imprestimo:
     print("Você não recebera o seu imprestimo")
     print("Felipe Bortoluzz dos Santos")
-if conta < imprestimo:
+if conta >= imprestimo:
     print("Você recebera o seu imprestimo")
     print("Felipe Bortoluzz dos Santos")
